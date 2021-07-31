7977461cfd6048ec9d0756b2f6f6d68bc4e51a64
MNAlfiansyah <MNAlfiansyah33@gmail.com>
2021-07-31 21:48:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shoes Store</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css" integrity="sha512-usVBAd66/NpVNfBge19gws2j6JZinnca12rAe2l+d+QkLU9fiG02O1X8Q6hepIpr/EYKZvKx/I9WsnujJuOmBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@0;1&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <div><span class="head-title"><img src="./assets/image/shoes-logo.png" />Shoes Store</span></div>
+        <div><span class="buy-now"><a href='#' class="head-title">Shop Now</a><span></div>
+        <div><span><a href='#' class="head-title">About Us</a><span></div>
+    </div>
+    <div class="content">
+        <div class="img-utama1">
+        </div>
+        <div class="img-couple">
+            <div><img src="https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"></div>
+            <div><p>Style Your Life</p></div>
+            <div><img src="https://images.pexels.com/photos/4277507/pexels-photo-4277507.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></div>
+        </div>
+        <div class="row img-vendor-logo pt-4">
+            <div class="col-4">
+                <div class="rounded-vendor">
+                    
+                </div>
+            </div>
+            <div class="col-4">
+                test
+            </div>
+            <div class="col-4">
+                test
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footer-content">
+
+        </div>
+        <div class="footer-copy">
+            <p>Privacy Policy     |     Terms and Conditions © 2021 ShoesStore</p>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.min.js" integrity="sha512-a6ctI6w1kg3J4dSjknHj3aWLEbjitAXAjLDRUxo2wyYmDFRcz2RJuQr5M3Kt8O/TtUSp8n2rAyaXYy1sjoKmrQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
